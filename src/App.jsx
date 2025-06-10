65ed9678b14686fa36bf7c02f938741782fef40f
qsrdev <matteo.albertini747@gmail.com>
2025-06-10 16:40:27 +0200

    featk: task endend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "./App.css";
 
 function App() {
   const apiUrl = "https://lanciweb.github.io/demo/api/actors";
@@ -18,19 +17,24 @@ function App() {
     <>
       <div className="container">
         <h1 className="text-center">Stars of Hollywood</h1>
-        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 my-3">
+        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 my-3 ">
           {actors.map((curActor) => (
-            <div className="card ">
-              <img src={curActor.image} className="card-img-top" alt={curActor.name} />
-              <div className="card-body">
-                <h5 className="card-title">{curActor.name}</h5>
-                <h6 className="card-subtitle mb-2 text-body-secondary">
-                  {curActor.nationality} - {curActor.birth_year}
-                </h6>
-                <p className="card-text">{curActor.biography}</p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
+            <div key={curActor.id} className="col">
+              <div className="card mb-3 h-100">
+                <img src={curActor.image} className="img-fluid rounded-top w-100 object-fit-cover" style={{ height: "300px" }} alt={`snapshot of ${curActor.name}`} />
+                <div className="card-body">
+                  <h5 className="card-title">{curActor.name}</h5>
+                  <h6 className="card-subtitle mb-2 text-body-secondary">
+                    {curActor.nationality} - {curActor.birth_year}
+                  </h6>
+                  <p className="card-text">{curActor.biography}</p>
+                </div>
+                <h5 className="text-center">Known For</h5>
+                {curActor.known_for.map((curAward) => (
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">{curAward}</li>
+                  </ul>
+                ))}
               </div>
             </div>
           ))}
